60f6d49495d02d2f17ef5cdee8c32670c39b2488
janice14381 <janice_czh@yahoo.com.sg>
2021-05-30 16:53:17 +0800

    bus router get info from API

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { Text, View, StyleSheet, TouchableOpacity, ActivityIndicator } from 'react-native';
+
 
 export default function App() {
+  const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=83139";
+  const [loading, setLoading] = useState(true);
+  const [arrival, setArrival] = useState("");
+  const [nxtarrival, setNextArrival] = useState("");
+
+  function loadBusStopData(){
+    fetch(BUSSTOP_URL)
+    .then((response) => {return response.json();})
+    .then((responseData) => {
+        const myBus = responseData.services.filter(
+              (item) => item.no === "155")[0];
+             
+        setArrival((parseInt(myBus.next.duration_ms)/1000).toFixed(0));
+        setNextArrival((myBus.next2.time));
+        setLoading(false);
+        
+    })
+  }
+
+    useEffect(()=>{
+        const interval = setInterval(loadBusStopData,1500);
+        return () => clearInterval(interval)
+    },[]);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.paragraph}>Bus Arrival Time</Text>
+      <Text style={{margin:50,color:"grey"}}>{loading?<ActivityIndicator size="large" color="orange" />: arrival} Sec</Text>
+      <Text style={styles.paragraph}>Next Arrival Time</Text>
+      <Text style={{margin:50,color:"grey"}}>{loading?<ActivityIndicator size="large" color="orange" />: nxtarrival}</Text>
+
+      <TouchableOpacity onPress={loadBusStopData}>
+        <Text style={styles.button}>refresh</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +45,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  button:{
+    padding: 20,
+    margin:5,
+    backgroundColor: 'black',
+    color: 'yellow',
+    borderRadius: 20,
+    fontWeight: 'bold'
+  }
 });
+
